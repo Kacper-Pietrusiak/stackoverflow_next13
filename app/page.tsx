53d436c1829e3bac56d6f524f171348147cd5e4e
KacperPietrusiak <kacperpietrusiak.bizes@gmail.com>
2023-10-31 12:45:35 +0100

    creating routes

--- app/page.tsx
+++
@@ -1,9 +0,0 @@
-export default function Home() {
-  return (
-   <div>
-      <h1 className="h1-bold">blah blah blah</h1>
-      <h2 className="h2-bold">blah blah blah</h2>
-      <h3 className="h3-semibold">blah blah blah</h3>
-   </div>
-  );
-}
