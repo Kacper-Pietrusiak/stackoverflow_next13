ec4c9b0007e43643e52ca4bcb6c5ac58978a3327
Kacper_Pietrusiak <kacperpietrusiak.biznes@gmail.com>
2023-11-13 08:39:15 +0100

    add auth

--- app/(root)/ask-questions/page.tsx
+++ app/(root)/ask-questions/page.tsx
@@ -1,9 +1,7 @@
-import React from 'react'
+import React from "react";
 
 const AskQuestions = () => {
-  return (
-    <div>AskQuestions</div>
-  )
-}
+  return <div>AskQuestions</div>;
+};
 
-export default AskQuestions
+export default AskQuestions;
